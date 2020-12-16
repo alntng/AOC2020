2344c85fe8735fc7248213181875a0618c5e1607
Alan <atang8013@gmail.com>
2020-12-16 00:08:45 -0500

    part 2

--- day15.js
+++ day15.js
@@ -7,7 +7,7 @@ const day15 = (nums) => {
   });
   nums.push(0);
 
-  for (let i = nums.length - 1; i < 2019; i++) {
+  for (let i = nums.length - 1; i < 6059; i++) {
     let lastSeen = seen[nums[i]];
     if (lastSeen !== undefined) {
       // console.log(i - lastSeen);
@@ -18,8 +18,34 @@ const day15 = (nums) => {
       seen[nums[i]] = nums.length - 2;
     }
   }
-  // console.log(seen);
-  console.log(nums[2019]);
+  // console.log(Object.keys(seen).length);
+  console.log(nums[(30000000 % 2020) - 1]);
 };
 
-day15([16, 1, 0, 18, 12, 14, 19]);
+// day15([16, 1, 0, 18, 12, 14, 19]);
+// console.log(30000000 / 2020);
+
+const starting = [0, 3, 6];
+
+let spoken = new Map();
+let justSpoke = 0;
+for (let i = 0; i < 5; i++) {
+  if (starting[i] || starting[i] === 0) {
+    justSpoke = starting[i];
+    spoken.set(justSpoke, i + 1);
+  } else if (!spoken.has(justSpoke)) {
+    spoken.set(justSpoke, i);
+    justSpoke = 0;
+  } else {
+    console.log(`just spoke ${justSpoke}`);
+    let temp = spoken.get(justSpoke);
+    console.log("temp", temp);
+    spoken.set(justSpoke, i);
+    console.log(i);
+    console.log("just set", spoken);
+    justSpoke = i - temp;
+    console.log(`just spoke after ${justSpoke}`);
+  }
+  console.log("=====");
+}
+console.log(justSpoke);

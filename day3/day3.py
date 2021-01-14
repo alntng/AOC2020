bd50f1b183a66651a651c5c3b3f5a8e6e063bc77
Alan <atang8013@gmail.com>
2021-01-14 12:06:56 -0500

    day 3 part 2 python

--- day3/day3.py
+++ day3/day3.py
@@ -14,4 +14,17 @@ def findTrees(field, right, down):
     return treeCount
 
 
-print(findTrees(input, 3, 1))
+def partTwo(field, slopes):
+    totalTrees = 1
+    for slope in slopes:
+        right = slope[0]
+        down = slope[1]
+
+        totalTrees *= findTrees(field, right, down)
+    return totalTrees
+
+
+restofSlopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+# print(findTrees(input, 3, 1))
+print(partTwo(input, restofSlopes))
